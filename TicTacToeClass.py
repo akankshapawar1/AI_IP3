1f214bd0bbe894b47c47a5febd8ab6eea29f7d13
Akanksha Pawar <42142815+akankshapawar1@users.noreply.github.com>
2023-11-05 01:19:13 -0400

    70 percent done

---
+++ TicTacToeClass.py
@@ -0,0 +1,395 @@
+from collections import namedtuple
+import numpy as np
+import random
+
+GameState = namedtuple('GameState', 'to_move, utility, board, moves')
+
+
+# ______________________________________________________________________________
+# Monte Carlo tree node and ucb function
+
+class MCT_Node:
+    """Node in the Monte Carlo search tree, keeps track of the children states."""
+
+    def __init__(self, parent=None, state=None, U=0, N=0):
+        self.__dict__.update(parent=parent, state=state, U=U, N=N)
+        self.children = {}
+        self.actions = None
+
+
+def ucb(n, C=1.4):
+    return np.inf if n.N == 0 else n.U / n.N + C * np.sqrt(np.log(n.parent.N) / n.N)
+
+
+def vector_add(a, b):
+    """Component-wise addition of two vectors."""
+    if not (a and b):
+        return a or b
+    if hasattr(a, '__iter__') and hasattr(b, '__iter__'):
+        assert len(a) == len(b)
+        return list(map(vector_add, a, b))
+    else:
+        try:
+            return a + b
+        except TypeError:
+            raise Exception('Inputs must be in the same size!')
+
+
+def minmax_decision(state, game):
+    """Given a state in a game, calculate the best move by searching
+    forward all the way to the terminal states. [Figure 5.3]"""
+
+    player = game.to_move(state)
+
+    def max_value(state):
+        if game.terminal_test(state):
+            return game.utility(state, player)
+        v = -np.inf
+        for a in game.actions(state):
+            v = max(v, min_value(game.result(state, a)))
+        return v
+
+    def min_value(state):
+        if game.terminal_test(state):
+            return game.utility(state, player)
+        v = np.inf
+        for a in game.actions(state):
+            v = min(v, max_value(game.result(state, a)))
+        return v
+
+    # Body of minmax_decision:
+    return max(game.actions(state), key=lambda a: min_value(game.result(state, a)))
+
+
+def alpha_beta_search(state, game):
+    """Search game to determine best action; use alpha-beta pruning.
+    As in [Figure 5.7], this version searches all the way to the leaves."""
+
+    player = game.to_move(state)
+
+    # Functions used by alpha_beta
+    def max_value(state, alpha, beta):
+        if game.terminal_test(state):
+            return game.utility(state, player)
+        v = -np.inf
+        for a in game.actions(state):
+            v = max(v, min_value(game.result(state, a), alpha, beta))
+            if v >= beta:
+                return v
+            alpha = max(alpha, v)
+        return v
+
+    def min_value(state, alpha, beta):
+        if game.terminal_test(state):
+            return game.utility(state, player)
+        v = np.inf
+        for a in game.actions(state):
+            v = min(v, max_value(game.result(state, a), alpha, beta))
+            if v <= alpha:
+                return v
+            beta = min(beta, v)
+        return v
+
+    # Body of alpha_beta_search:
+    best_score = -np.inf
+    beta = np.inf
+    best_action = None
+    for a in game.actions(state):
+        v = min_value(game.result(state, a), best_score, beta)
+        if v > best_score:
+            best_score = v
+            best_action = a
+    return best_action
+
+
+def random_player(game, state):
+    """A player that chooses a legal move at random."""
+    return random.choice(game.actions(state)) if game.actions(state) else None
+
+
+# Heuristic alpha-beta search
+def alpha_beta_cutoff_search(state, game, d=4, cutoff_test=None, eval_fn=None):
+    """Search game to determine best action; use alpha-beta pruning.
+    This version cuts off search and uses an evaluation function."""
+
+    player = game.to_move(state)
+
+    # Functions used by alpha_beta
+    def max_value(state, alpha, beta, depth):
+        if cutoff_test(state, depth):
+            return eval_fn(state)
+        v = -np.inf
+        for a in game.actions(state):
+            v = max(v, min_value(game.result(state, a), alpha, beta, depth + 1))
+            if v >= beta:
+                return v
+            alpha = max(alpha, v)
+        return v
+
+    def min_value(state, alpha, beta, depth):
+        if cutoff_test(state, depth):
+            return eval_fn(state)
+        v = np.inf
+        for a in game.actions(state):
+            v = min(v, max_value(game.result(state, a), alpha, beta, depth + 1))
+            if v <= alpha:
+                return v
+            beta = min(beta, v)
+        return v
+
+    # Body of alpha_beta_cutoff_search starts here:
+    # The default test cuts off at depth d or at a terminal state
+    cutoff_test = (cutoff_test or (lambda state, depth: depth > d or game.terminal_test(state)))
+    eval_fn = eval_fn or (lambda state: game.utility(state, player))
+    best_score = -np.inf
+    beta = np.inf
+    best_action = None
+    for a in game.actions(state):
+        v = min_value(game.result(state, a), best_score, beta, 1)
+        if v > best_score:
+            best_score = v
+            best_action = a
+    return best_action
+
+
+def monte_carlo_tree_search(state, game, N=1000):
+    def select(n):
+        """select a leaf node in the tree"""
+        if n.children:
+            return select(max(n.children.keys(), key=ucb))
+        else:
+            return n
+
+    def expand(n):
+        """expand the leaf node by adding all its children states"""
+        if not n.children and not game.terminal_test(n.state):
+            n.children = {MCT_Node(state=game.result(n.state, action), parent=n): action
+                          for action in game.actions(n.state)}
+        return select(n)
+
+    def simulate(game, state):
+        """simulate the utility of current state by random picking a step"""
+        player = game.to_move(state)
+        while not game.terminal_test(state):
+            action = random.choice(list(game.actions(state)))
+            state = game.result(state, action)
+        v = game.utility(state, player)
+        return -v
+
+    def backprop(n, utility):
+        """passing the utility back to all parent nodes"""
+        if utility > 0:
+            n.U += utility
+        # if utility == 0:
+        #     n.U += 0.5
+        n.N += 1
+        if n.parent:
+            backprop(n.parent, -utility)
+
+    root = MCT_Node(state=state)
+
+    for _ in range(N):
+        leaf = select(root)
+        child = expand(leaf)
+        result = simulate(game, child.state)
+        backprop(child, result)
+
+    max_state = max(root.children, key=lambda p: p.N)
+
+    return root.children.get(max_state)
+
+
+def query_player(game, state):
+    """Make a move by querying standard input."""
+    print("current state:")
+    game.display(state)
+    print("available moves: {}".format(game.actions(state)))
+    print("")
+    move = None
+    if game.actions(state):
+        move_string = input('Your move? ')
+        try:
+            move = eval(move_string)
+        except NameError:
+            move = move_string
+    else:
+        print('no legal moves: passing turn to next player')
+    return move
+
+
+'''class Game:
+
+    def __init__(self):
+        self.initial = None
+        self.current_state = None
+
+    def to_move(self, state):
+        """Returns the player whose move it is in this state."""
+        return state.to_move
+
+    def play_game(self, *players):
+        """Play an n-person, move-alternating game."""
+        state = self.initial
+        while True:
+            for player in players:
+                move = player(state)
+                state = self.result(state, move)
+                if self.terminal_test(state):
+                    self.display(state)
+                    return self.utility(state, self.to_move(self.initial))
+
+    def utility(self, state, param):
+        raise NotImplementedError
+
+    def terminal_test(self, state):
+        raise NotImplementedError
+
+    def display(self, state):
+        raise NotImplementedError
+
+    def result(self, state, move):
+        raise NotImplementedError
+'''
+
+class Game:
+    """A game is similar to a problem, but it has a utility for each
+    state and a terminal test instead of a path cost and a goal
+    test. To create a game, subclass this class and implement actions,
+    result, utility, and terminal_test. You may override display and
+    successors or you can inherit their default methods. You will also
+    need to set the .initial attribute to the initial state; this can
+    be done in the constructor."""
+
+    def __init__(self):
+        self.initial = GameState(to_move='X', utility=0, board={}, moves=0)
+        self.current_state = None
+
+    def actions(self, state):
+        """Return a list of the allowable moves at this point."""
+        raise NotImplementedError
+
+    def result(self, state, move):
+        """Return the state that results from making a move from a state."""
+        raise NotImplementedError
+
+    def utility(self, state, player):
+        """Return the value of this final state to player."""
+        raise NotImplementedError
+
+    def terminal_test(self, state):
+        """Return True if this is a final state for the game."""
+        return not self.actions(state)
+
+    def to_move(self, state):
+        """Return the player whose move it is in this state."""
+        return state.to_move
+
+    def display(self, state):
+        """Print or otherwise display the state."""
+        print(state)
+
+    def __repr__(self):
+        return '<{}>'.format(self.__class__.__name__)
+
+    '''def play_game(self, *players):
+        """Play an n-person, move-alternating game."""
+        state = self.initial
+        while True:
+            for player in players:
+                move = player(self, state)
+                state = self.result(state, move)
+                if self.terminal_test(state):
+                    self.display(state)
+                    # print('utility')
+                    # print(self.utility(state, self.to_move(self.initial)))
+                    return self.utility(state, self.to_move(self.initial))'''
+
+    def play_game(self, *players):
+        """Play an n-person, move-alternating game."""
+        state = self.initial
+        while True:
+            for player in players:
+                move = player(self, state)
+                state = self.result(state, move)
+                self.display(state)
+
+                # Determine the player's symbol ('X' or 'O')
+                player_symbol = 'O' if player == players[1] else 'X'
+                # Print the utility for the player who just made the move
+                print(f"Player {player_symbol}'s Utility: {state.utility if player_symbol == 'X' else -state.utility}")
+
+                if self.terminal_test(state):
+                    # The game is over, so print the final utilities
+                    # print(f"Final Utilities: Player X: {state.utility}, Player O: {-state.utility}")
+                    return state.utility
+
+
+class TicTacToe(Game):
+    """Play TicTacToe on an h x v board, with Max (first player) playing 'X'.
+    A state has the player to move, a cached utility, a list of moves in
+    the form of a list of (x, y) positions, and a board, in the form of
+    a dict of {(x, y): Player} entries, where Player is 'X' or 'O'."""
+
+    def __init__(self, h=3, v=3, k=3):
+        super().__init__()
+        self.h = h
+        self.v = v
+        self.k = k
+        moves = [(x, y) for x in range(1, h + 1)
+                 for y in range(1, v + 1)]
+        self.initial = GameState(to_move='X', utility=0, board={}, moves=moves)
+
+    def actions(self, state):
+        """Legal moves are any square not yet taken."""
+        return state.moves
+
+    def result(self, state, move):
+        if move not in state.moves:
+            return state  # Illegal move has no effect
+        board = state.board.copy()
+        board[move] = state.to_move
+        moves = list(state.moves)
+        moves.remove(move)
+        return GameState(to_move=('O' if state.to_move == 'X' else 'X'),
+                         utility=self.compute_utility(board, move, state.to_move),
+                         board=board, moves=moves)
+
+    def utility(self, state, player):
+        """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
+        return state.utility if player == 'X' else -state.utility
+
+    def terminal_test(self, state):
+        """A state is terminal if it is won or there are no empty squares."""
+        return state.utility != 0 or len(state.moves) == 0
+
+    def display(self, state):
+        board = state.board
+        for x in range(1, self.h + 1):
+            for y in range(1, self.v + 1):
+                print(board.get((x, y), '.'), end=' ')
+            print()
+
+    def compute_utility(self, board, move, player):
+        """If 'X' wins with this move, return 1; if 'O' wins return -1; else return 0."""
+        if (self.k_in_row(board, move, player, (0, 1)) or
+                self.k_in_row(board, move, player, (1, 0)) or
+                self.k_in_row(board, move, player, (1, -1)) or
+                self.k_in_row(board, move, player, (1, 1))):
+            return +1 if player == 'X' else -1
+        else:
+            return 0
+
+    def k_in_row(self, board, move, player, delta_x_y):
+        """Return true if there is a line through move on board for player."""
+        (delta_x, delta_y) = delta_x_y
+        x, y = move
+        n = 0  # n is number of moves in row
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x + delta_x, y + delta_y
+        x, y = move
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x - delta_x, y - delta_y
+        n -= 1  # Because we counted move itself twice
+        return n >= self.k
--- TicTacToeGameApp.py
+++ TicTacToeGameApp.py
@@ -11,17 +11,29 @@ Could not find your player, please try again: 1
 Please enter your second player: 7
 Could not find your player, please try again: 6
 """
+from TicTacToeClass import random_player, minmax_decision, alpha_beta_search, alpha_beta_cutoff_search, monte_carlo_tree_search, \
+    query_player, Game, TicTacToe
 
 playersName = ['1. Random Player', '2. MiniMax Player', '3. Alpha Beta Player', '4. Heuristic Alpha Beta Player',
                '5. MCTS Player', '6. Query Player']
-players = [1, 2, 3, 4, 5, 6]
+
+validPlayerList = [1, 2, 3, 4, 5, 6]
+
+player_functions = {
+    1: random_player,
+    2: minmax_decision,
+    3: alpha_beta_search,
+    4: alpha_beta_cutoff_search,
+    5: monte_carlo_tree_search,
+    6: query_player
+}
 
 
-def main():
+'''def main():
     def getPlayer(prompt):
         while True:
             player = int(input(prompt))
-            if player in players:
+            if player in validPlayerList:
                 return player
             else:
                 prompt = "Could not find your player, please try again: "
@@ -29,11 +41,114 @@ def main():
     print("Player Selection: ")
     for p in playersName:
         print(p)
+
     while True:
-        player1 = getPlayer('Please enter your first player:')
-        player2 = getPlayer('Please enter your second player:')
-        print("TTTC will be called now")
+        player1_choice = getPlayer('Please enter your first player:')
+        player2_choice = getPlayer('Please enter your second player:')
+
+        TTT = TicTacToe()
+        TTT.current_state = TTT.initial
+
+        # create player instance
+        player1 = player_functions[player1_choice]
+        player2 = player_functions[player2_choice]
+
+        # start
+        scores = {'Player1': 0, 'Player2': 0}
+        for round in range(1, 4):  # This loop will go through rounds 1 to 3
+            print(f"\nRound {round}:")
+
+            # initializing the game with two players
+            # print("TicTacToe will be called now")
+            winner = TTT.play_game(player1, player2)
+
+            # Update the scores based on who won this round
+
+            if winner == 1:
+                scores['Player1'] += 1  # Player1 is 'X' and has won
+            elif winner == -1:
+                scores['Player2'] += 1  # Player2 is 'O' and has won
+
+            # Check if a player has won two rounds, then break the loop
+            if scores['Player1'] == 2 or scores['Player2'] == 2:
+                break
+
+        # Print the final scores and winner
+        print(f"\nFinal Scores: Player1 - {scores['Player1']}, Player2 - {scores['Player2']}")
+        if scores['Player1'] > scores['Player2']:
+            print("Player1 wins the game!")
+        elif scores['Player1'] < scores['Player2']:
+            print("Player2 wins the game!")
+        else:
+            print("The game is a draw!")
+        # end
+
         user_continue_choice = input('Would you like to play the game again? ')
+        if user_continue_choice.lower() == 'no':
+            print('Thank You for Playing Our Game!')
+            break'''
+
+def main():
+    def getPlayer(prompt):
+        while True:
+            player = int(input(prompt))
+            if player in validPlayerList:
+                return player
+            else:
+                prompt = "Could not find your player, please try again: "
+
+    print("Player Selection: ")
+    for p in playersName:
+        print(p)
+
+    while True:
+        player1_choice = getPlayer('Please enter your first player:')
+        player2_choice = getPlayer('Please enter your second player:')
+
+        TTT = TicTacToe()
+        TTT.current_state = TTT.initial
+
+        # create player instance
+        player1 = player_functions[player1_choice]
+        player2 = player_functions[player2_choice]
+
+        # start
+        scores = {'Player1': 0, 'Player2': 0}
+        last_round_was_draw = False  # Keep track of whether the last round was a draw
+        for round in range(1, 4):  # This loop will go through rounds 1 to 3
+            print(f"\nRound {round}:")
+
+            # initializing the game with two players
+            winner = TTT.play_game(player1, player2)
+
+            # Update the scores based on who won this round
+            if winner == 1:
+                scores['Player1'] += 1  # Player1 is 'X' and has won
+                last_round_was_draw = False  # Reset draw tracker on a win
+            elif winner == -1:
+                scores['Player2'] += 1  # Player2 is 'O' and has won
+                last_round_was_draw = False  # Reset draw tracker on a win
+            else:
+                if last_round_was_draw:  # Check if the last round was also a draw
+                    print("Two consecutive draws. Game will stop now.")
+                    break  # End the game if two consecutive draws occurred
+                last_round_was_draw = True  # Set draw tracker for this round
+
+            # Check if a player has won two rounds, then break the loop
+            if scores['Player1'] == 2 or scores['Player2'] == 2:
+                break
+
+        # Print the final scores and winner
+        print(f"\nFinal Scores: Player1 - {scores['Player1']}, Player2 - {scores['Player2']}")
+        if scores['Player1'] > scores['Player2']:
+            print("Player1 wins the game!")
+        elif scores['Player1'] < scores['Player2']:
+            print("Player2 wins the game!")
+        else:
+            print("The game is a draw!")
+        # end
+
+        user_continue_choice = input('Would you like to play the game again? (yes/no): ')
         if user_continue_choice.lower() == 'no':
             print('Thank You for Playing Our Game!')
             break
